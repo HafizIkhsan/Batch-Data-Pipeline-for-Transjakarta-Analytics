ba59585edfdb5cb2f4bb4ffbc1172103e5017a64
Hafiz Ikhsan <hafizikhsan73@gmail.com>
2026-02-26 20:44:05 +0700

    env

--- src/config.py
+++ src/config.py
@@ -1,10 +1,14 @@
 from sqlalchemy import create_engine
+from dotenv import load_dotenv
+import os
 
-DB_USER = "admin"
-DB_PASSWORD = "admin"
-DB_HOST = "localhost"
-DB_PORT = "5433"
-DB_NAME = "warehouse"
+load_dotenv()
+
+DB_USER = os.getenv("DB_USER")
+DB_PASSWORD = os.getenv("DB_PASSWORD")
+DB_HOST = os.getenv("DB_HOST")
+DB_PORT = os.getenv("DB_PORT")
+DB_NAME = os.getenv("DB_NAME")
 
 DATABASE_URL = f"postgresql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
 
